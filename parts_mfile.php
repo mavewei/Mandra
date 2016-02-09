dd930cf8cdc4e0fd77468b9751d0ba95458833e4
mavewei <mavewei@gmail.com>
2016-02-09 16:53:01 +0800

    Add Pagination

--- parts_mfile.php
+++ parts_mfile.php
@@ -51,6 +51,62 @@ if($sid == $_SESSION['SID']) {
 			$result = mysql_query($query);
 			$rows = mysql_num_rows($result);
 			if(!$result) die ("Table access failed: " . mysql_error());
+			/**
+				Pagination
+			**/
+
+			/**
+				number of rows to show per page.
+			**/
+			$rowsPerPage = 8;
+			/**
+				find out total pages
+			**/
+			$totalPages = ceil($rows / $rowsPerPage);
+			/**
+				get the current page or set a default
+			**/
+			if(isset($_GET['currentPage']) && is_numeric($_GET['currentPage'])) {
+				/**
+					cast var as int
+				**/
+				$currentPage = (int) $_GET['currentPage'];
+			} else {
+				/**
+					default page num
+				**/
+				$currentPage = 1;
+			}
+			/**
+				if current page is greater than total pages
+			**/
+			if($currentPage > $totalPages) {
+				/**
+					set current page to last page
+				**/
+				$currentPage = $totalPages;
+			}
+			/**
+				if current page is less than first page
+			**/
+			if($currentPage < 1) {
+				/**
+					set current page to first page
+				**/
+				$currentPage = 1;
+			}
+			/**
+				The offset of the list, based on current page
+			**/
+			$offset = ($currentPage - 1) * $rowsPerPage;
+			/**
+				get the info from the db
+			**/
+			$query = "SELECT * FROM partsMasterFile WHERE status = 'Active' ORDER BY id DESC LIMIT $offset, $rowsPerPage";
+
+			$result = mysql_query($query);
+			$rows = mysql_num_rows($result);
+			// $result = mysql_query($sql, $conn) or trigger_error("SQL", E_USER_ERROR);
 		} else {
 			/**
 				Redirect to dashboard if not Superuser or Manager
@@ -93,29 +149,55 @@ if($sid == $_SESSION['SID']) {
 				</li>
 			</ul>
 			<div class="row margin-top-10">
-				<div class="col-md-1"></div>
-				<div class="col-md-10">
+				<div class="col-md-2"></div>
+				<div class="col-md-8">
 					<div class="portlet light">
 						<div class="portlet-title">
 							<div class="caption">
 								<span class="caption-subjet font-green-sharp bold uppercase">Search</span>
 							</div>
+							<div class="actions btn-set">
+								<a class="btn" href=""><i class="glyphicon glyphicon-refresh"></i></a>
+							</div>
 						</div>
 						<div class="portlet-body">
 							<div class="row">
-								<div class="col-md-3"></div>
-								<div class="col-md-3"></div>
-								<div class="col-md-3"></div>
-								<div class="col-md-3"></div>
+								<div class="col-md-4">
+									<label class="control-label label-space">By Parts Number</label>
+									<div class="input-group">
+										<input type="text" class="form-control input-sm" id="searchPartsNumber" name="searchPartsNumber" placeholder="By Parts Number">
+										<span class="input-group-btn">
+											<button class="btn blue btn-sm" onclick="searchPartsNumber();">Go!</button>
+										</span>
+									</div>
+								</div>
+								<div class="col-md-4">
+									<label class="control-label label-space">By Description</label>
+									<div class="input-group">
+										<input type="text" class="form-control input-sm" id="searchDescription" name="searchDescription" placeholder="By Description">
+										<span class="input-group-btn">
+											<button class="btn blue btn-sm" onclick="searchDescription();">Go!</button>
+										</span>
+									</div>
+								</div>
+								<div class="col-md-4">
+									<label class="control-label label-space">By Brand</label>
+									<div class="input-group">
+										<input type="text" class="form-control input-sm" id="searchBrand" name="searchBrand" placeholder="By Brand">
+										<span class="input-group-btn">
+											<button class="btn blue btn-sm" onclick="searchBrand();">Go!</button>
+										</span>
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-				<div class="col-md-1"></div>
+				<div class="col-md-2"></div>
 			</div>
 			<div class="row">
-				<div class="col-md-1"></div>
-				<div class="col-md-10">
+				<div class="col-md-2"></div>
+				<div class="col-md-8">
 					<div class="portlet light">
 						<div class="portlet-title">
 							<div class="caption">
@@ -127,15 +209,15 @@ if($sid == $_SESSION['SID']) {
 							<div class="tools"></div>
 						</div>
 						<div class="portlet-body">
-							<div id="slimScrollParts">
-								<div class="table-scrollable table-scrollable-borderless">
+							<!-- <div id="slimScrollParts"> -->
+								<div id="searchDiv" class="table-scrollable table-scrollable-borderless">
 									<table class="table table-hover table-light">
 										<?php
 										if($rows > 0) {
 											echo "<thead><tr class='uppercase'><th colspan='2'>S/N</th>";
-											echo "<th class='left th-width-15'>Parts Number</th>";
-											echo "<th class='left th-width-15'>Description</th>";
-											echo "<th class='center th-width-15'>Date Created</th>";
+											echo "<th class='left'>Parts Number</th>";
+											echo "<th class='left'>Description</th>";
+											echo "<th class='center'>Date Created</th>";
 											echo "</tr></thead><tbody>";
 											for($j = 0; $j < $rows; ++$j) {
 												$partsId = ucfirst(mysql_result($result, $j, 'partsId'));
@@ -159,19 +241,71 @@ if($sid == $_SESSION['SID']) {
 											echo "</tbody>";
 										} else {
 											/**
-											 No users account.
+											 No master file.
 											 **/
 											echo "<div class='block' style='height:100%'><div class='centered-users'>";
-											echo "<h3 class='no-users'>No parts master file found!</h3></tbody></div></div>";
+											echo "<h3 class='no-users'>No parts master file found!</h3></div></div>";
 										}
 										?>
 									</table>
+									<nav style="text-align: center; <?php if($rows < 1) echo 'display: none' ?>">
+										<ul class="pagination">
+											<?php
+											/**
+												Pagination: http:\/\/www\.phpfreaks.com\/tutorial/basic-pagination
+											**/
+											if($currentPage == 1) {
+												echo "<li class='disabled'>";
+												echo "<a href='#' aria-label='Previous'";
+												echo "<span aria-hidden='true'>&laquo;</span>";
+												echo "</a></li>";
+											} else {
+												echo "<li>";
+												echo "<a href='{$_SERVER['PHP_SELF']}?currentPage=1' aria-label='Previous'>";
+												echo "<span aria-hidden='true'>&laquo;</span>";
+												echo "</a></li>";
+											}
+											/**
+												range of num links to show
+											**/
+											$range = 3;
+											/**
+												loop to show links to range of pages around current page
+											**/
+											for($x = ($currentPage - $range); $x < (($currentPage + $range) + 1); $x++) {
+												/**
+													if it's a valid page number...
+												**/
+												if(($x > 0) && ($x <= $totalPages)) {
+													/**
+														if we're on current page
+													**/
+													if($x == $currentPage) {
+														/**
+															'highlight' it but don't make a link
+														**/
+														echo "<li><a href='#'>$x</a></li>";
+													} else {
+														/**
+															make it a link
+														**/
+														echo "<li><a href='{$_SERVER['PHP_SELF']}?currentPage=$x'>$x</a></li>";
+													}
+												}
+											}
+											echo "<li>";
+											echo "<a href='{$_SERVER['PHP_SELF']}?currentPage=$totalPages'>&raquo;</a>";
+											echo "</li>";
+											?>
+										</ul>
+									</nav>
 								</div>
-							</div>
+
+							<!-- </div> -->
 						</div>
 					</div>
 				</div>
-				<div class="col-md-1"></div>
+				<div class="col-md-2"></div>
 			</div>
 		</div>
 	</div>
@@ -190,33 +324,58 @@ $(function() {
 		})
 	})
 })
-/**
-   Bootbox alert customize.
-**/
-/*
-$(function() {
-	$('.logoutAlert').click(function(){
-		bootbox.confirm("Are you sure you want to LOGOUT?", function(result) {
-			if(result) {
-				window.location = "logout.php";
-			}
+function searchPartsNumber() {
+	var searchPartsNumber = document.getElementById("searchPartsNumber").value;
+	if(searchPartsNumber) {
+		$.ajax({
+			type: 'post',
+			url: 'search_data.php',
+			data: {
+				searchPartsNumber: searchPartsNumber,
+			},
+			success: function (response) {
+				$('#searchDiv').html(response);
+            }
 		});
-	})
-})
-*/
+	} else {}
+}
+function searchDescription() {
+	var searchDescription = document.getElementById("searchDescription").value;
+	if(searchDescription) {
+		$.ajax({
+			type: 'post',
+			url: 'search_data.php',
+			data: {
+				searchDescription: searchDescription,
+			},
+			success: function (response) {
+				$('#searchDiv').html(response);
+            }
+		});
+	} else {}
+}
+function searchBrand() {
+	var searchBrand = document.getElementById("searchBrand").value;
+	if(searchBrand) {
+		$.ajax({
+			type: 'post',
+			url: 'search_data.php',
+			data: {
+				searchBrand: searchBrand,
+			},
+			success: function (response) {
+				$('#searchDiv').html(response);
+            }
+		});
+	} else {}
+}
 $(function(){
-	$('#slimScrollParts').slimScroll({
-	    		height: '325px',
-    		});
-/*
     var row = <?php echo $rows; ?>;
 	if(row < 7) {	} else {
 		$('#slimScrollParts').slimScroll({
-	    		height: '325px',
-	    		touchScrollStep: 50
+	    		height: '325px'
     		});
 	}
-*/
 });
 </script>
 <?php include('pages/page_footer.php'); ?>
