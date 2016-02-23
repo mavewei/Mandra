4e017dfbfeb165aa6ed5ea5009ca4f6141e0ace6
mavewei <mavewei@gmail.com>
2016-02-23 23:47:49 +0800

    Add New Table Structure

--- index.php
+++ index.php
@@ -249,6 +249,68 @@
 						// idZeroFill INT(5) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT,
 		$result = mysql_query($query);
 		if(!$result) die ("Tables create failed: " . mysql_error());
+		// Parts Master File - Uom
+		// IMPORTANT: Please use "dateTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP" if timezone setting are correct.
+		$query = "CREATE TABLE partsUom (
+						id INT NOT NULL AUTO_INCREMENT,
+						dateTime TIMESTAMP NULL,
+						partsUomId VARCHAR(15) NOT NULL,
+						partsUomName VARCHAR(50) NOT NULL,
+						status VARCHAR(15) NULL,
+						PRIMARY KEY (id))";
+		$result = mysql_query($query);
+		if(!$result) die ("Tables create failed: " . mysql_error());
+		// Parts Master File - Category
+		// IMPORTANT: Please use "dateTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP" if timezone setting are correct.
+		$query = "CREATE TABLE partsCategory (
+						id INT NOT NULL AUTO_INCREMENT,
+						dateTime TIMESTAMP NULL,
+						partsCategoryId VARCHAR(15) NOT NULL,
+						partsCategoryName VARCHAR(50) NOT NULL,
+						status VARCHAR(15) NULL,
+						PRIMARY KEY (id))";
+		$result = mysql_query($query);
+		if(!$result) die ("Tables create failed: " . mysql_error());
+		// Parts Master File - Brand
+		// IMPORTANT: Please use "dateTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP" if timezone setting are correct.
+		$query = "CREATE TABLE partsBrand (
+						id INT NOT NULL AUTO_INCREMENT,
+						dateTime TIMESTAMP NULL,
+						partsBrandId VARCHAR(15) NOT NULL,
+						partsBrandName VARCHAR(50) NOT NULL,
+						status VARCHAR(15) NULL,
+						PRIMARY KEY (id))";
+		$result = mysql_query($query);
+		if(!$result) die ("Tables create failed: " . mysql_error());
+		// Parts Master File - Model
+		// IMPORTANT: Please use "dateTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP" if timezone setting are correct.
+		$query = "CREATE TABLE partsModel (
+						id INT NOT NULL AUTO_INCREMENT,
+						dateTime TIMESTAMP NULL,
+						partsModelId VARCHAR(15) NOT NULL,
+						partsModelName VARCHAR(50) NOT NULL,
+						status VARCHAR(15) NULL,
+						PRIMARY KEY (id))";
+		$result = mysql_query($query);
+		if(!$result) die ("Tables create failed: " . mysql_error());
+		// Parts Master File - Equipment Type
+		// IMPORTANT: Please use "dateTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP" if timezone setting are correct.
+		$query = "CREATE TABLE partsEquipType (
+						id INT NOT NULL AUTO_INCREMENT,
+						dateTime TIMESTAMP NULL,
+						partsEquipTypeId VARCHAR(15) NOT NULL,
+						partsEquipTypeName VARCHAR(50) NOT NULL,
+						status VARCHAR(15) NULL,
+						PRIMARY KEY (id))";
+		$result = mysql_query($query);
+		if(!$result) die ("Tables create failed: " . mysql_error());
+
+
+
+
+
+
+
 		/**
 		 supplierLISTS
 		 **/
@@ -464,10 +526,104 @@
 						('Liberia', 'Lofa'), ('Liberia', 'Margibi'), ('Liberia', 'Maryland'), ('Liberia', 'Grand Montserrado'),
 						('Liberia', 'Nimba'), ('Liberia', 'Rivercess'), ('Liberia', 'River Gee'), ('Liberia', 'Sinoe'),
 						('Malaysia', 'Johor'), ('Malaysia', 'Kedah'), ('Malaysia', 'Kelantan'), ('Malaysia', 'Kuala Lumpur'),
-						('Malaysia', 'Labuan'), ('Malaysia', 'Melaka'), ('Malaysia', 'Negeri Sembilan'), ('Malaysia', 'Pahang'),
-						('Malaysia', 'Perak'), ('Malaysia', 'Perlis'), ('Malaysia', 'Penang'), ('Malaysia', 'Sabah'),
-						('Malaysia', 'Sarawak'), ('Malaysia', 'Selangor'), ('Malaysia', 'Terengganu')";
+						('Malaysia', 'Labuan'), ('Malaysia', 'Melaka'), ('Malaysia', 'Negeri Sembilan'),
+						('Malaysia', 'Pahang'), ('Malaysia', 'Perak'), ('Malaysia', 'Perlis'), ('Malaysia', 'Penang'),
+						('Malaysia', 'Sabah'), ('Malaysia', 'Sarawak'), ('Malaysia', 'Selangor'),
+						('Malaysia', 'Terengganu')";
 		*/
+		$result = mysql_query($query);
+		if(!$result) die ("Tables access failed: " . mysql_error());
+
+		// Parts Uom details
+		$query = "INSERT INTO partsUom
+						(dateTime, partsUomId, partsUomName, status)
+					VALUES
+						('$time', 'PU001', 'Bags', 'Active'), ('$time', 'PU002', 'Bails', 'Active'),
+						('$time', 'PU003', 'Bales', 'Active'), ('$time', 'PU004', 'Books', 'Active'),
+						('$time', 'PU005', 'Bottles', 'Active'), ('$time', 'PU006', 'Boxes', 'Active'),
+						('$time', 'PU007', 'Buckets', 'Active'), ('$time', 'PU008', 'Bundles', 'Active'),
+						('$time', 'PU009', 'Cans', 'Active'), ('$time', 'PU010', 'Cartons', 'Active'),
+						('$time', 'PU011', 'Coils', 'Active'), ('$time', 'PU012', 'Cups', 'Active'),
+						('$time', 'PU013', 'Dozens', 'Active'), ('$time', 'PU014', 'Feet', 'Active'),
+						('$time', 'PU015', 'Gallons', 'Active'), ('$time', 'PU016', 'Kgs', 'Active'),
+						('$time', 'PU017', 'Litres', 'Active'), ('$time', 'PU018', 'Meters', 'Active'),
+						('$time', 'PU019', 'Packs', 'Active'), ('$time', 'PU020', 'Pairs', 'Active'),
+						('$time', 'PU021', 'Pcs', 'Active'), ('$time', 'PU022', 'Quarts', 'Active'),
+						('$time', 'PU023', 'Reams', 'Active'), ('$time', 'PU024', 'Rolls', 'Active'),
+						('$time', 'PU025', 'Sets', 'Active'), ('$time', 'PU026', 'Sheets', 'Active'),
+						('$time', 'PU027', 'Suits', 'Active'), ('$time', 'PU028', 'Tins', 'Active'),
+						('$time', 'PU029', 'Units', 'Active'), ('$time', 'PU030', 'Yards', 'Active')";
+		$result = mysql_query($query);
+		if(!$result) die ("Tables access failed: " . mysql_error());
+		// Parts Category details
+		$query = "INSERT INTO partsCategory
+						(dateTime, partsCategoryId, partsCategoryName, status)
+					VALUES
+						('$time', 'PC001', 'Asset', 'Active'), ('$time', 'PC002', 'Battery', 'Active'),
+						('$time', 'PC003', 'Bolt & Nut', 'Active'), ('$time', 'PC004', 'Camp Supply', 'Active'),
+						('$time', 'PC005', 'Electrical Supply', 'Active'), ('$time', 'PC006', 'Gears', 'Active'),
+						('$time', 'PC007', 'General Supply', 'Active'), ('$time', 'PC008', 'Lubricant & Oil', 'Active'),
+						('$time', 'PC009', 'Oil Seal', 'Active'), ('$time', 'PC010', 'Parts', 'Active'),
+						('$time', 'PC011', 'Parts Book', 'Active'), ('$time', 'PC012', 'Production Supply', 'Active'),
+						('$time', 'PC013', 'Stationary', 'Active'), ('$time', 'PC014', 'Tools', 'Active'),
+						('$time', 'PC015', 'Tyre', 'Active'), ('$time', 'PC016', 'Workshop Supply', 'Active')";
+		$result = mysql_query($query);
+		if(!$result) die ("Tables access failed: " . mysql_error());
+		// Parts Brand details
+		$query = "INSERT INTO partsBrand
+						(dateTime, partsBrandId, partsBrandName, status)
+					VALUES
+						('$time', 'PB001', 'Beiben Mecedez', 'Active'), ('$time', 'PB002', 'Camings', 'Active'),
+						('$time', 'PB003', 'CASE', 'Active'), ('$time', 'PB004', 'CAT', 'Active'),
+						('$time', 'PB005', 'Chine White', 'Active'), ('$time', 'PB006', 'DAYUN', 'Active'),
+						('$time', 'PB007', 'Dong Feng', 'Active'), ('$time', 'PB008', 'Good Year', 'Active'),
+						('$time', 'PB009', 'ISUZU', 'Active'), ('$time', 'PB010', 'Kama', 'Active'),
+						('$time', 'PB011', 'Komatsu', 'Active'), ('$time', 'PB012', 'Mercedez', 'Active'),
+						('$time', 'PB013', 'Mitsubishi', 'Active'), ('$time', 'PB014', 'Nissan', 'Active'),
+						('$time', 'PB015', 'Perkins', 'Active'), ('$time', 'PB016', 'Rhino', 'Active'),
+						('$time', 'PB017', 'SAE', 'Active'), ('$time', 'PB018', 'SEM', 'Active'),
+						('$time', 'PB019', 'Shan Tui', 'Active'), ('$time', 'PB020', 'Suzuki', 'Active'),
+						('$time', 'PB021', 'Toyota', 'Active'), ('$time', 'PB022', 'XG3200S', 'Active'),
+						('$time', 'PB023', 'Xu Gong', 'Active')";
+		$result = mysql_query($query);
+		if(!$result) die ("Tables access failed: " . mysql_error());
+		// Parts Model details
+		$query = "INSERT INTO partsModel
+						(dateTime, partsModelId, partsModelName, status)
+					VALUES
+						('$time', 'PM001', '528B', 'Active'), ('$time', 'PM002', 'Beiben 2541KZ', 'Active'),
+						('$time', 'PM003', 'C6121', 'Active'), ('$time', 'PM004', 'CASE 580SL', 'Active'),
+						('$time', 'PM005', 'CAT 140G', 'Active'), ('$time', 'PM006', 'CAT 528', 'Active'),
+						('$time', 'PM007', 'CAT 962G', 'Active'), ('$time', 'PM008', 'CAT 966C', 'Active'),
+						('$time', 'PM009', 'CAT D6G', 'Active'), ('$time', 'PM010', 'CAT D7G', 'Active'),
+						('$time', 'PM011', 'DY125-B', 'Active'), ('$time', 'PM012', 'EQ1258KB', 'Active'),
+						('$time', 'PM013', 'Mitsubishi L200', 'Active'), ('$time', 'PM014', 'Montero', 'Active'),
+						('$time', 'PM015', 'Nissan Frontier', 'Active'), ('$time', 'PM016', 'Nissan March', 'Active'),
+						('$time', 'PM017', 'Nissan V8', 'Active'), ('$time', 'PM018', 'PC200-6', 'Active'),
+						('$time', 'PM019', 'SC8DK230Q3', 'Active'), ('$time', 'PM020', 'SD16', 'Active'),
+						('$time', 'PM021', 'SD22', 'Active'), ('$time', 'PM022', 'SEM 660B', 'Active'),
+						('$time', 'PM023', 'TACOMA', 'Active'), ('$time', 'PM024', 'Toyota 4 Runner', 'Active'),
+						('$time', 'PM025', 'Toyota Fortuna', 'Active'), ('$time', 'PM026', 'Toyota Hilux', 'Active'),
+						('$time', 'PM027', 'Toyota Land Cruiser', 'Active'), ('$time', 'PM028', 'TS654', 'Active')";
+		$result = mysql_query($query);
+		if(!$result) die ("Tables access failed: " . mysql_error());
+		// Parts Equipment Type details
+		$query = "INSERT INTO partsEquipType
+						(dateTime, partsEquipTypeId, partsEquipTypeName, status)
+					VALUES
+						('$time', 'PQ001', 'Air Compressor', 'Active'), ('$time', 'PQ002', 'Backhoe', 'Active'),
+						('$time', 'PQ003', 'Bulldozer', 'Active'), ('$time', 'PQ004', 'Chain Saw', 'Active'),
+						('$time', 'PQ005', 'Crane', 'Active'), ('$time', 'PQ006', 'Cutting Machine', 'Active'),
+						('$time', 'PQ007', 'Dump Truck', 'Active'), ('$time', 'PQ008', 'Excavator', 'Active'),
+						('$time', 'PQ009', 'Farm Tractor', 'Active'), ('$time', 'PQ010', 'Forklift', 'Active'),
+						('$time', 'PQ011', 'Fuel Tanker', 'Active'), ('$time', 'PQ012', 'Gasoline Car', 'Active'),
+						('$time', 'PQ013', 'Generator', 'Active'), ('$time', 'PQ014', 'Jeep', 'Active'),
+						('$time', 'PQ015', 'Lathe Machine', 'Active'), ('$time', 'PQ016', 'Logging Truck', 'Active'),
+						('$time', 'PQ017', 'Lorry', 'Active'), ('$time', 'PQ018', 'Low Bed', 'Active'),
+						('$time', 'PQ019', 'Motor Bike', 'Active'), ('$time', 'PQ020', 'Motor Grader', 'Active'),
+						('$time', 'PQ021', 'Pickup', 'Active'), ('$time', 'PQ022', 'Radio', 'Active'),
+						('$time', 'PQ023', 'Skid Tanker', 'Active'), ('$time', 'PQ024', 'Skidder', 'Active'),
+						('$time', 'PQ025', 'Small Car', 'Active'), ('$time', 'PQ026', 'Wheel Loader', 'Active')";
 		$result = mysql_query($query);
 		if(!$result) die ("Tables access failed: " . mysql_error());
 		/**
