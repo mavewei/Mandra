165405ba7f83b9524e5582ba5b3f299b52b6f7de
mavewei <mavewei@gmail.com>
2016-02-23 23:46:55 +0800

    Add Back Disable Function

--- pages/page_meta.php
+++ pages/page_meta.php
@@ -1,3 +1,9 @@
+<script type = "text/javascript">
+	history.pushState(null, null, '');
+	window.addEventListener('popstate', function(event) {
+		history.pushState(null, null, '');
+	});
+</script>
 <title>Mandra Forestry Liberia Limited - Management Information System.</title>
 </head>
 <body>
